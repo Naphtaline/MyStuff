0444224a8d8fc10c4ddbbaf911b3461af7a8d616
Napth <napth@pop-os.localdomain>
2022-03-03 20:22:17 +0100

    Wemos relay : V1.0

--- esp8266/Wemos_relay/src/main.cpp
+++ esp8266/Wemos_relay/src/main.cpp
@@ -1,15 +1,57 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
-int relay = 13;
+int relay = 1;
 
-void setup() {
-  pinMode(relay, OUTPUT);
+ESP8266WebServer server(80);
+ 
+
+void handleRootPath() {
+  server.send(200, "text/plain", "Dead simple http server");
 }
 
-void loop() {
+void handleOnPath() {
   digitalWrite(relay, HIGH);
-  delay(1000);
-  digitalWrite(relay, LOW);
-  delay(1000);
+  server.send(200, "text/plain", "on");
+}
 
+void handleOffPath() {
+  digitalWrite(relay, LOW);
+  server.send(200, "text/plain", "off");
+}
+
+void setup() {
+ 
+  Serial.begin(115200);
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, LOW);
+  WiFi.begin("Bbox-A259B8D5", "rantanplan");  //Connect to the WiFi network
+ 
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for connection
+ 
+    delay(500);
+    Serial.println("Waiting to connect…");
+ 
+  }
+ 
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //Print the local IP
+ 
+  server.on("/other", []() {   //Define the handling function for the path
+ 
+    server.send(200, "text / plain", "Other URL");
+ 
+  });
+ 
+  server.on("/", handleRootPath);    //Associate the handler function to the path
+  server.on("/relayon", handleOnPath);    //Associate the handler function to the path
+  server.on("/relayoff", handleOffPath);    //Associate the handler function to the path
+  server.begin();                    //Start the server
+  Serial.println("Server listening");
+ 
+}
+ 
+void loop() {
+  server.handleClient();         //Handling of incoming requests
 }
